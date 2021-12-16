f4bf83d2869ed152b226bb5e1bc66032a2f2ab94
Felipe de Souza <systen3@gmail.com>
2021-12-16 07:57:13 -0300

    ponto inicial e movimentacao de disparo

--- js/js.js
+++ js/js.js
@@ -11,7 +11,7 @@ function start() { // Inicio da função start()
 
 
 
-
+var podeAtirar=true;
 var velocidade=5;
 var posicaoY = parseInt(Math.random() * 334);	
 var jogo = {}
@@ -85,6 +85,7 @@ function movefundo() {
 		if (jogo.pressionou[TECLA.D]) {
 			
 			//Chama função Disparo	
+			disparo();
 		}
 	
 		} // fim da função movejogador()
@@ -126,6 +127,40 @@ function movefundo() {
 			}
 	
 	} // fim da função moveamigo()
+
+	function disparo() {
+	
+		if (podeAtirar==true) {
+			
+		podeAtirar=false;
+		
+		topo = parseInt($("#jogador").css("top"))
+		posicaoX= parseInt($("#jogador").css("left"))
+		tiroX = posicaoX + 190;
+		topoTiro=topo+37;
+		$("#fundoGame").append("<div id='disparo'></div");
+		$("#disparo").css("top",topoTiro);
+		$("#disparo").css("left",tiroX);
+		
+		var tempoDisparo=window.setInterval(executaDisparo, 30);
+		
+		} //Fecha podeAtirar
+	 
+			   function executaDisparo() {
+			posicaoX = parseInt($("#disparo").css("left"));
+			$("#disparo").css("left",posicaoX+15); 
+	
+					if (posicaoX>900) {
+							
+				window.clearInterval(tempoDisparo);
+				tempoDisparo=null;
+				$("#disparo").remove();
+				podeAtirar=true;
+						
+					   }
+		} // Fecha executaDisparo()
+	} // Fecha disparo()
+	
 	
 	
 	
